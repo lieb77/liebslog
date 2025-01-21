b216aef1a0b6ba51c9008fd42628c8d82552de97
Paul Lieberman <lieb77@gmail.com>
2025-01-21 04:43:59 +0000

    Ran phpcbf. It said it fixed 400+ errors. I think it just reformatted everything

--- src/GetPosts.php
+++ src/GetPosts.php
@@ -11,68 +11,74 @@ use Drupal\Core\Url;
 use Drupal\Core\Link;
 
 
-class GetPosts {
-	protected $posts;
-	protected $entityTypeManager;
-		
-	public function __construct(EntityTypeManagerInterface $entityTypeManager ) {
-	
-		$this->entityTypeManager = $entityTypeManager;
+class GetPosts
+{
+    protected $posts;
+    protected $entityTypeManager;
+        
+    public function __construct(EntityTypeManagerInterface $entityTypeManager )
+    {
+    
+        $this->entityTypeManager = $entityTypeManager;
 
-	
-		$storage = $entityTypeManager->getStorage('node');
-		$ids = $storage->getQuery()
-			->condition('type', 'blog')
-			->accessCheck('TRUE')
-			->sort('created', 'DESC')
-			->range(0, 10)
-			->execute();
+    
+        $storage = $entityTypeManager->getStorage('node');
+        $ids = $storage->getQuery()
+            ->condition('type', 'blog')
+            ->accessCheck('TRUE')
+            ->sort('created', 'DESC')
+            ->range(0, 10)
+            ->execute();
 
-		foreach ($ids as $bid) {
-		  $bname = Node::load($bid)->getTitle();
-		  $this->posts[$bid] = $bname;
-	
-		}
-	}
-	
-	/**
- 	* {@inheritdoc}
-   	*/
-  	public static function create(ContainerInterface $container) {
-    	return new static(      		
-      		 $container->get('entity_type.manager')
-    	);
-  	}
-	
-	public function getPosts() {
-		return $this->posts;
-	}
-	
-	public function getPost($nid){
-		$node =  Node::load($nid);		
-		
-		// Get the title
-		$title = $node->getTitle();
-		
-		// Get the post summary
-		// $view_builder = $this->entityTypeManager->getViewBuilder('node');
-		// $teaser = $view_builder->view($node, 'teaser');
-		//$teaser = $node->body->view('teaser');
-		$teaser = $node->get('body')->summary;
-		// Get the link
-		$link  = Link::fromTextAndUrl("Read on Lieb's Log",
-				$node->toUrl()->setAbsolute()				
-				)->toString();
+        foreach ($ids as $bid) {
+            $bname = Node::load($bid)->getTitle();
+            $this->posts[$bid] = $bname;
+    
+        }
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public static function create(ContainerInterface $container)
+    {
+        return new static(              
+            $container->get('entity_type.manager')
+        );
+    }
+    
+    public function getPosts()
+    {
+        return $this->posts;
+    }
+    
+    public function getPost($nid)
+    {
+        $node =  Node::load($nid);        
+        
+        // Get the title
+        $title = $node->getTitle();
+        
+        // Get the post summary
+        // $view_builder = $this->entityTypeManager->getViewBuilder('node');
+        // $teaser = $view_builder->view($node, 'teaser');
+        //$teaser = $node->body->view('teaser');
+        $teaser = $node->get('body')->summary;
+        // Get the link
+        $link  = Link::fromTextAndUrl(
+            "Read on Lieb's Log",
+            $node->toUrl()->setAbsolute()                
+        )->toString();
 
-		$link = $node->toUrl()->setAbsolute()->toString();
-		
-		return [
-			'title' => $title,
-			'text'	=> $teaser,
-			'link'  => $link,
-			];
+        $link = $node->toUrl()->setAbsolute()->toString();
+        
+        return [
+        'title' => $title,
+        'text'    => $teaser,
+        'link'  => $link,
+        ];
 
-	}
-	
+    }
+    
 }//End of clss
-	
+    
